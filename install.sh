367226f81a9cb03d454cc87665301ddf9dd616e2
royingantaginting <roy.i.ginting@gdplabs.id>
2014-12-22 15:25:52 +0700

    Make process uninteractive

--- install.sh
+++ install.sh
@@ -103,6 +103,7 @@ function process_args {
 			--node=*) SLAVE_NODE="${1#*=}" ;;
 			--user=*) MASTER_USER=${1#*=} ;;
 			--master=*) MASTER=${1#*=} ;;
+			--token=*) SLAVE_TOKEN=${1#*=} ;;
 			--java-args=*) JAVA_ARGS="${1#*=}" ;;
 		esac
 		shift
@@ -146,135 +147,10 @@ function configure_daemon {
 	echo
 	echo "${MASTER_USER}'s API token is required to authenticate a JNLP slave."
 	echo "The API token is listed at ${MASTER}${MASTER_HTTP_PORT}/user/${MASTER_USER}/configure"
-	read -p "API token for ${MASTER_USER}: " SLAVE_TOKEN
 	while ! curl -L --url ${MASTER}${MASTER_HTTP_PORT}/user/${MASTER_USER} --user ${MASTER_USER}:${SLAVE_TOKEN} --insecure --silent --head --fail --output /dev/null ; do
 		echo "Unable to authenticate ${MASTER_USER} with this token"
 		read -p "API token for ${MASTER_USER}: " SLAVE_TOKEN
 	done
-	OSX_KEYCHAIN_PASS=${OSX_KEYCHAIN_PASS:-`env LC_CTYPE=C tr -dc "a-zA-Z0-9-_" < /dev/urandom | head -c 20`}
-	create_keychain
-	sudo -i -u ${SERVICE_USER} ${SERVICE_WRKSPC}/security.sh set-password --password=${SLAVE_TOKEN} --account=${MASTER_USER} --service=\"${SLAVE_NODE}\"
-	KEYSTORE_PASS=`sudo -i -u ${SERVICE_USER} ${SERVICE_WRKSPC}/security.sh get-password --account=${SERVICE_USER} --service=java_truststore`
-	KEYSTORE_PASS=${KEYSTORE_PASS:-`env LC_CTYPE=C tr -dc "a-zA-Z0-9-_" < /dev/urandom | head -c 20`}
-	sudo -i -u ${SERVICE_USER} ${SERVICE_WRKSPC}/security.sh set-password --password=${KEYSTORE_PASS} --account=${SERVICE_USER} --service=java_truststore
-	if [ "$PROTOCOL" == "https" ]; then
-		echo "
-If the certificate for ${MASTER_NAME} is not trusted by Java, you will need 
-to install public certificates required for Java to trust ${MASTER_NAME}.
-NOTE: The installer is not capable of testing that Java trusts ${MASTER_NAME}.
-
-If ${MASTER_NAME} has a self-signed certifate, the public certificate
-must be imported. If the certificate for ${MASTER_NAME} is signed by
-a certificate authority, you may need to import both the root and server CA
-certificates.
-
-To install certificates, you will need to:
-1) copy or download the certificates into ${SERVICE_HOME}
-2) use the following command:
-sudo -i -u ${SERVICE_USER} ${SERVICE_WRKSPC}/security.sh add-java-certificate \
---alias=AN_ALIAS --certificate=/path/to/certificate
-If the certificate is a Root CA cert, add the --ca-cert flag to the above
-command.
-"
-	fi
-	create_ssh_keys
-	configure_github
-	echo
-	echo "
-If you need to do additional tasks to setup ${SERVICE_USER}, you can
-sudo -i -u ${SERVICE_USER}
-in Terminal to open a shell running as ${SERVICE_USER}
-"
-}
-
-function create_ssh_keys {
-	if [ ! -f ${SERVICE_HOME}/.ssh/id_rsa ]; then
-		echo "
-Do you wish to create SSH keys for this ${SERVICE_USER}? These keys will be
-suitable for GitHub, amoung other services. Keys generated at this point will
-not be protected by a password.
-"
-		read -p "Create SSH keys? (yes/no) [yes]" CONFIRM
-		CONFIRM=${CONFIRM:-yes}
-		if [[ "${CONFIRM}" =~ ^[Yy] ]] ; then
-			sudo -i -u ${SERVICE_USER} ssh-keygen -t rsa -N \'\' -f ${SERVICE_HOME}/.ssh/id_rsa -C \"${SERVICE_USER}@${SLAVE_NODE}\"
-		fi
-		echo "
-You will need to connect to each SSH host as ${SERVICE_USER} to add the host
-to the known_hosts file to allow the service to use SSH. This can be done
-using the following command:
-sudo -i -u ${SERVICE_USER} ssh account@service
-
-To get ${SERVICE_USER}'s public key to add to a service to allow SSH:
-sudo -i -u ${SERVICE_USER} cat ${SERVICE_HOME}/.ssh/id_rsa.pub
-"
-	fi
-}
-
-function configure_github {
-	read -p "Will this slave need to connect to GitHub? (yes/no) [no]" CONFIRM
-	CONFIRM=${CONFIRM:-no}
-	if [[ "${CONFIRM}" =~ ^[Yy] ]] ; then
-		echo "Attempting to SSH to GitHub... You may be prompted to trust github.com."
-		sudo -i -u ${SERVICE_USER} ssh -T git@github.com
-		RESULT=$?
-		if [ $RESULT -eq 255 ] ; then
-			echo "
-You need to add the ssh keys to the GitHub account that Jenkins uses
-
-Copy the following key to https://github.com/settings/ssh after you have
-logged into GitHub as the user that Jenkins connects to GitHub as
-"
-			sudo -i -u ${SERVICE_USER} cat ${SERVICE_HOME}/.ssh/id_rsa.pub
-		fi
-	fi
-}
-
-function configure_adc {
-	read -p "Will this slave need Apple Developer Certificates? (yes/no) [yes]" CONFIRM
-	CONFIRM=${CONFIRM:-yes}
-	if [[ "${CONFIRM}" =~ ^[Yy] ]] ; then
-		echo "Importing WWDR intermediate certificate..."
-		sudo -i -u ${SERVICE_USER} curl  --silent -L --remote-name --url https://developer.apple.com/certificationauthority/AppleWWDRCA.cer
-		sudo -i -u ${SERVICE_USER} ${SERVICE_WRKSPC}/security.sh add-apple-certificate --certificate=${SERVICE_HOME}/AppleWWDRCA.cer
-		sudo -i rm ${SERVICE_HOME}/AppleWWDRCA.cer
-		echo "
-You will need to import your own developer certificates following these steps:
-1) Export the Certificate and Key from Keychain for your developer profiles.
-2) sudo cp /path/to/exported-keys-and-certificates ${SERVICE_HOME}
-3) For each certificate and key (this is a single multiline command):
-   sudo -i -u ${SERVICE_USER} ${SERVICE_WRKSPC}/security.sh \
-   add-apple-certificate --certificate=${SERVICE_HOME}/name-of-exported-cert
-"
-	fi
-}
-
-function create_keychain {
-	local KEYCHAINS=${SERVICE_HOME}/Library/Keychains
-	if [ ! -d ${KEYCHAINS} ]; then
-		sudo mkdir -p ${KEYCHAINS}
-		sudo chown -R ${SERVICE_USER}:${SERVICE_GROUP} ${KEYCHAINS}
-	fi
-	if [ ! -f ${KEYCHAINS}/${OSX_KEYCHAIN} ]; then
-		sudo -i -u ${SERVICE_USER} security create-keychain -p ${OSX_KEYCHAIN_PASS} ${OSX_KEYCHAIN}
-		if [ -f ${KEYCHAINS}/.keychain_pass ]; then
-			sudo chmod 666 ${KEYCHAINS}/.keychain_pass
-		fi
-		sudo chmod 777 ${KEYCHAINS}
-		sudo sh -c "echo 'OSX_KEYCHAIN_PASS=${OSX_KEYCHAIN_PASS}' > ${KEYCHAINS}/.keychain_pass"
-		sudo chown -R ${SERVICE_USER}:${SERVICE_GROUP} ${KEYCHAINS} 
-		sudo chmod 400 ${KEYCHAINS}/.keychain_pass
-		sudo chmod 755 ${KEYCHAINS}
-	fi
-	echo "
-The OS X Keychain password for ${SERVICE_USER} is ${OSX_KEYCHAIN_PASS}
-You will need to copy this into the Jenkins configuration on ${MASTER_NAME}
-for every project that will be compiled on this slave, or copy a special
-per-project Keychain to ${SERVICE_HOME}/Library/Keychains.
-
-Note that the login Keychain for ${SERVICE_USER} contains secrets needed for
-${SLAVE_NODE} to connect to ${MASTER_NAME}.
-"
 }
 
 function write_config {
@@ -314,16 +190,9 @@ and stopped using the command
 
 This service logs to /var/log/${SERVICE_USER}/org.jenkins-ci.slave.jnlp.log
 "
-	read -p "Start the slave service now (yes/no) [yes]? " CONFIRM
 	CONFIRM=${CONFIRM:-"yes"}
 	if [[ "${CONFIRM}" =~ ^[Yy] ]] ; then
 		sudo launchctl load -F /Library/LaunchDaemons/org.jenkins-ci.slave.jnlp.plist
-		echo
-		read -p "Open Console.app to view logs now (yes/no) [yes]? " CONFIRM
-		CONFIRM=${CONFIRM:-"yes"}
-		if [[ "${CONFIRM}" =~ ^[Yy] ]] ; then
-			open /var/log/${SERVICE_USER}/org.jenkins-ci.slave.jnlp.log
-		fi
 	fi
 }
 
@@ -350,25 +219,7 @@ function rawurlencode() {
 	REPLY="${encoded}"   #+or echo the result (EASIER)... or both... :p
 }
 
-echo "
-        _          _   _              _ _  _ _    ___   ___ _              
-     _ | |___ _ _ | |_(_)_ _  ___  _ | | \| | |  | _ \ / __| |__ ___ _____ 
-    | || / -_) ' \| / / | ' \(_-< | || | .\` | |__|  _/ \__ \ / _\` \ V / -_)
-     \__/\___|_||_|_\_\_|_||_/__/  \__/|_|\_|____|_|   |___/_\__,_|\_/\___|
 
-This script will download, install, and configure a Jenkins JNLP Slave on OS X.
-
-You must be an administrator on the system you are installing the Slave on,
-since this installer will add a user to the system and then configure the slave
-as that user.
-
-A Java Development Kit (JDK) must be installed prior to installing the Jenkins
-JNLP Slave.
-
-During the configuration, you will be prompted for necessary information. The
-suggested or default response will be in brackets [].
-"
-read -p "Continue (yes/no) [yes]? " CONFIRM
 CONFIRM=${CONFIRM:-"yes"}
 if [[ "${CONFIRM}" =~ ^[Yy] ]] ; then
 	echo
